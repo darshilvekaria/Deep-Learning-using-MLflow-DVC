26f18678602a0497bf32fc4b8ab1242eb7a9d173
Darshil Vekaria <darshilvekaria11@gmail.com>
2025-06-16 00:54:43 +0530

    updated code for MLflow tracking. Credentials are not added.

--- src/cnnClassifier/components/model_evaluation_mlflow.py
+++ src/cnnClassifier/components/model_evaluation_mlflow.py
@@ -5,6 +5,7 @@ import mlflow.keras
 from urllib.parse import urlparse
 from cnnClassifier.entity.config_entity import EvaluationConfig
 from cnnClassifier.utils.common import read_yaml, create_directories,save_json
+import os
 
 
 class Evaluation:
@@ -54,6 +55,9 @@ class Evaluation:
 
     
     def log_into_mlflow(self):
+        os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/MLFLOW_TRACKING_URI.mlflow"
+        os.environ["MLFLOW_TRACKING_USERNAME"]="MLFLOW_TRACKING_USERNAME"
+        os.environ["MLFLOW_TRACKING_PASSWORD"]="MLFLOW_TRACKING_PASSWORD"
         mlflow.set_registry_uri(self.config.mlflow_uri)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         
